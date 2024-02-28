8dcd61b7aeb87d7c6f789fa1128532144414eabf
codewithkevin <oseikelvin709@gmail.com>
2024-02-28 05:30:08 +0000

    update

--- src/payment/controller/purchaseBookController.ts
+++ src/payment/controller/purchaseBookController.ts
@@ -1,88 +1,127 @@
 import { Request, Response } from "express";
 import { BookTransactionService } from "../service/purchaseBookService";
-import { BookItem } from "../model/purchaseBookModel";
+import { BookItem, PaymentStatusEnums } from "../model/purchaseBookModel";
 import { BookService } from "../../books/service/book.service";
+import uniqid from "uniqid";
 
 const bookTransactionService = new BookTransactionService();
 const bookService = new BookService();
 
 export const initiateBookPurchase = async (req: Request, res: Response) => {
-  const { userId, books, paymentDetails } = req.body;
+  try {
+    const { userId, books, paymentDetails } = req.body;
 
-  const purchaseBookIds = books.map((book: BookItem) => book.bookId);
-  const booksFromDB = await bookService.getBooksByIds(purchaseBookIds);
-
-  const totalAmount = books.reduce((total: number, purchase: BookItem) => {
-    const foundBook = booksFromDB.find(
-      (book) => book._id.toString() === purchase.bookId.toString()
+    const totalAmount = await bookTransactionService.calculateTotalAmount(
+      books
     );
 
-    if (!foundBook) throw new Error("One of the books was not found");
+    paymentDetails.amount = totalAmount;
+    paymentDetails.meta = {
+      userId: userId,
+      books: books.map((item: BookItem) => ({
+        bookId: item.bookId,
+        quantity: item.quantity,
+      })),
+      totalAmount: totalAmount,
+    };
 
-    const currentPrice = foundBook.bookPrice * purchase.quantity;
+    const paymentReferenceId = uniqid();
 
-    return total + currentPrice;
-  }, 0);
-
-  paymentDetails.amount = totalAmount;
-  paymentDetails.meta = {
-    userId: userId,
-    books: books.map((item: BookItem) => ({
-      bookId: item.bookId,
-      quantity: item.quantity,
-    })),
-    totalAmount: totalAmount,
-  };
-
-  const paymentResponse = await bookTransactionService.initiatePayment(
-    paymentDetails
-  );
-
-  if (
-    paymentResponse.status === "success" &&
-    paymentResponse.meta.authorization.mode === "redirect"
-  ) {
-    return res.status(200).json({
-      message: "Please complete the CAPTCHA verification",
-      redirectUrl: paymentResponse.meta.authorization.redirect,
-    });
-  } else if (paymentResponse.status === "success") {
-    const transactionId = paymentResponse.meta.authorization.transaction_id;
-
-    const verificationResponse = await bookTransactionService.verifyPayment(
-      transactionId
+    const paymentResponse = await bookTransactionService.initiatePayment(
+      paymentReferenceId,
+      paymentDetails
     );
 
-    if (verificationResponse.status === "success") {
-      const transactionData = {
-        user: userId,
-        books: books.map((item: BookItem) => ({
-          bookId: item.bookId,
-          quantity: item.quantity,
-        })),
-        paymentMode: "mobileMoney",
-        totalAmount: totalAmount,
-      };
+    const updatedBooks = await Promise.all(
+      books.map(async (bookItem: any) => {
+        const bookDetails = await bookService.getBookById(bookItem.bookId);
 
-      // Pass this object to the createTransaction method.
-      const savedTransaction = await bookTransactionService.createTransaction(
-        transactionData
+        if (!bookDetails) return null;
+
+        return {
+          book: {
+            id: bookItem.bookId,
+            title: bookDetails.title,
+            description: bookDetails.description,
+            price: {
+              currency: bookDetails.bookCurrency,
+              amount: bookDetails.bookPrice,
+            },
+          },
+          quantity: bookItem.quantity,
+        };
+      })
+    );
+
+    const bookPaymentData = {
+      userId: userId,
+      paymentReferenceId: paymentReferenceId,
+      status: PaymentStatusEnums.PENDING,
+      totalAmount: {
+        currency: paymentDetails.currency,
+        amount: totalAmount,
+      },
+      books: updatedBooks,
+      paymentDate: new Date().toISOString().split("T")[0],
+      paymentDateTime: new Date().toISOString(),
+      paymentHistory: [
+        {
+          statusFrom: PaymentStatusEnums.PENDING,
+          statusTo: PaymentStatusEnums.PENDING,
+          date: new Date().toISOString(),
+        },
+      ],
+    };
+
+    if (
+      paymentResponse.status === "success" &&
+      paymentResponse.meta?.authorization.mode === "redirect"
+    ) {
+      await bookTransactionService.createTransaction(bookPaymentData);
+      return res.status(200).json({
+        message: "Please complete the payment verification",
+        redirectUrl: paymentResponse.meta.authorization.redirect,
+      });
+    } else if (paymentResponse.status === "success") {
+      const transactionId = paymentResponse.data.id;
+
+      const verificationResponse = await bookTransactionService.verifyPayment(
+        transactionId
       );
 
-      return res.status(200).json({
-        message: "Books purchased successfully",
-        data: savedTransaction,
-      });
+      if (verificationResponse.status === "success") {
+        const transactionData = {
+          user: userId,
+          books: books,
+          paymentMode: paymentDetails.paymentMode,
+          totalAmount: totalAmount,
+        };
+
+        const savedTransaction = await bookTransactionService.createTransaction(
+          transactionData
+        );
+
+        return res.status(200).json({
+          message: "Books purchased successfully",
+          data: savedTransaction,
+        });
+      } else {
+        return res.status(400).json({
+          message: "Payment verification failed",
+          details: verificationResponse,
+        });
+      }
     } else {
       return res.status(400).json({
-        message: "Payment verification failed",
-        details: verificationResponse,
+        message: "Payment initiation failed",
+        details: paymentResponse,
       });
     }
-  } else {
-    return res.status(400).json({
-      message: "Payment initiation failed",
-      details: paymentResponse,
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({
+      message: "An error occurred during the book purchase process",
+      error: error.message,
     });
   }
 };
--- src/payment/model/purchaseBookModel.ts
+++ src/payment/model/purchaseBookModel.ts
@@ -1,45 +1,128 @@
-import mongoose, { Schema, Document, Types } from "mongoose";
+import mongoose, { Types } from "mongoose";
 import { BookDocument } from "../../books/model/book.model";
 import { AuthDocument } from "../../auth/model/auth.model";
 
-//todo: add buyerName, phone_number, email, created_at, transaction Id
+// create enum for payment status
+export enum PaymentStatusEnums {
+  PENDING = "pending",
+  SUCCESS = "successful",
+  FAILED = "failed",
+}
+
+export const PaymentStatus = [
+  PaymentStatusEnums.PENDING,
+  PaymentStatusEnums.SUCCESS,
+  PaymentStatusEnums.FAILED,
+];
+
+type PaymentHistory = {
+  statusFrom: PaymentStatusEnums;
+  statusTo: PaymentStatusEnums;
+  date: string;
+};
+
 export interface BookItem {
   bookId: Types.ObjectId | BookDocument;
   quantity: number;
 }
 
-export interface TransactionData {
-  user: Types.ObjectId | string;
-  books: BookItem[];
-  paymentMode: string;
-  totalAmount: number;
-}
-export interface TransactionDocument extends Document {
+export interface TransactionDocument {
   _id: string;
-  user: Types.ObjectId | AuthDocument;
-  books: BookItem[];
-  paymentMode: string;
-  totalAmount: number;
-  transactionDate?: Date;
+  userId: Types.ObjectId | AuthDocument;
+  paymentReferenceId: string;
+  status: PaymentStatusEnums;
+  totalAmount: {
+    currency: string;
+    amount: number;
+  };
+  paymentDetails?: {
+    transactionId: string;
+    paymentMode: string;
+    transactionIp: string;
+    paymentStatus: PaymentStatusEnums;
+    paymentType: string;
+    accountId: string;
+    paymentDate: string;
+    userName: string;
+    phoneNumber: string;
+    email: string;
+    currency: string;
+    amount: number;
+  };
+  books: {
+    books: BookDocument;
+    quantity: number;
+  }[];
+  paymentDate: string;
+  paymentDateTime: string;
+  paymentHistory?: PaymentHistory[];
 }
 
-const BookItemSchema = new Schema<BookItem>({
-  bookId: { type: Schema.Types.ObjectId, ref: "Book", required: true },
-  quantity: { type: Number, required: true },
+const PaymentHistorySchema = new mongoose.Schema<PaymentHistory>({
+  statusFrom: { required: true, type: String, enum: PaymentStatus },
+  statusTo: { required: true, type: String, enum: PaymentStatus },
+  date: { required: true, type: String },
 });
 
-const TransactionSchema = new Schema<TransactionDocument>(
+const paymentDetailsSchema = new mongoose.Schema({
+  transactionId: { required: true, type: String },
+  paymentMode: { required: true, type: String },
+  transactionIp: { required: true, type: String },
+  paymentStatus: { required: true, type: String, enum: PaymentStatus },
+  paymentType: { required: true, type: String },
+  accountId: { required: true, type: String },
+  paymentDate: { required: true, type: String },
+  userName: { required: true, type: String },
+  phoneNumber: { required: true, type: String },
+  email: { required: true, type: String },
+  currency: { required: true, type: String },
+  amount: { required: true, type: Number },
+});
+
+const bookSchema = new mongoose.Schema({
+  id: { required: true, type: String },
+  title: { required: true, type: String },
+  description: { required: true, type: String },
+  price: {
+    currency: { required: true, type: String },
+    amount: { required: true, type: Number },
+  },
+});
+
+const BookQuantitySchema = new mongoose.Schema<{
+  book: BookDocument;
+  quantity: number;
+}>(
   {
-    user: { type: Schema.Types.ObjectId, ref: "Auth", required: true },
-    books: [BookItemSchema],
-    paymentMode: { type: String, required: true },
-    totalAmount: { type: Number, required: true },
-    transactionDate: { type: Date, default: Date.now },
+    book: { required: true, type: bookSchema },
+    quantity: { required: true, type: Number },
+  },
+  { _id: false }
+);
+
+const schema = new mongoose.Schema<TransactionDocument>(
+  {
+    userId: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      index: true,
+      ref: "User",
+    },
+    status: { required: true, type: String, enum: PaymentStatus, index: true },
+    paymentReferenceId: { required: true, type: String, index: true },
+    totalAmount: {
+      currency: { required: true, type: String },
+      amount: { required: true, type: Number },
+    },
+    paymentDetails: paymentDetailsSchema,
+    books: [BookQuantitySchema],
+    paymentDate: { required: true, type: String, index: true },
+    paymentDateTime: { required: true, type: String },
+    paymentHistory: [PaymentHistorySchema],
   },
   { timestamps: true }
 );
 
-export const TransactionModel = mongoose.model<TransactionDocument>(
-  "BookTransaction",
-  TransactionSchema
-);
+schema.index({ paymentDate: 1, status: 1 });
+
+export const TransactionModel = mongoose.model("Transaction", schema);
