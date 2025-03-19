858cd16ef392d46771b1f86c87614386a5bda613
codewithkevin <oseikelvin709@gmail.com>
2025-03-19 05:42:07 +0000

    update

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
-  port: 1337,
-  dbUri: "mongodb://localhost:27017/aws-store",
+  port: 1537,
+  dbUri: "mongodb+srv://readerApp:g6vVSetBqAScskNV@cluster0ne.biqbk.mongodb.net/",
   secretKey: "awsStoreNewBackend",
   saltWorkFactor: 10,
   accessTokenTtl: "1440m",
